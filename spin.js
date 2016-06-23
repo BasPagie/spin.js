ce894a5af3a5aa64c7b626b0c2acf843956bd063
BasPagie <baspagie@hotmail.com>
2016-06-23 11:10:29 +0200

    Update spin.js

--- spin.js
+++ spin.js
@@ -1,26 +1,26 @@
 function $(select){
-	var self = {};
-	self.select = select;
-	self.element = document.querySelector(self.select);
+	var self = {}; //Maakt variabele aan om te gebruiken
+	self.select = select; 
+	self.element = document.querySelector(self.select); 
 
 	self.spin = function(argument) {
-		var rotated = false;
+		var rotated = false; 
 
-		self.element.onclick = function() {
+		self.element.onclick = function() { //na een klik van de gebruiker
 		    var div = self.element,
-		        deg = rotated ? 0 : 360;
+		        deg = rotated ? 0 : 360; //definieerd de graden die gedraaid moeten worden
 
-		    div.style.webkitTransform = 'rotate('+deg+'deg)'; 
+		    div.style.webkitTransform = 'rotate('+deg+'deg)'; //JS gegevens worden verwerkt in CSS3 code
 		    div.style.mozTransform    = 'rotate('+deg+'deg)'; 
 		    div.style.msTransform     = 'rotate('+deg+'deg)'; 
 		    div.style.oTransform      = 'rotate('+deg+'deg)'; 
 		    div.style.transform       = 'rotate('+deg+'deg)'; 
-		    div.style.webkitTransition = 'all 1s ease-in-out';
+		    div.style.webkitTransition = 'all 1s ease-in-out'; 
 			div.style.mozTransition = 'all 1s ease-in-out';
 			div.style.oTransition = 'all 1s ease-in-out';
 			div.style.transition = 'all 1s ease-in-out';
 
-		    rotated = !rotated;
+		    rotated = !rotated; //reset de rotatie (volgende klik is de rotatie andersom)
 		}	
 	}
 	return self;
